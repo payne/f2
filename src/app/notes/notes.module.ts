4d04cb768d4a70b20d91496c482bd3a32f684308
Matt Payne <Payne@MattPayne.org>
2019-01-19 18:35:36 -0600

    Woot! Angular MatTableModule wiring is not throwing errors.  Next up -- data!

--- src/app/notes/notes.module.ts
+++ src/app/notes/notes.module.ts
@@ -6,11 +6,29 @@ import { NotesListComponent } from './notes-list/notes-list.component';
 import { NoteDetailComponent } from './note-detail/note-detail.component';
 import { NotesService } from './notes.service';
 import { DataTableComponent } from '../data-table/data-table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { 
+  MatTableModule, 
+  MatFormFieldModule, 
+  MatInputModule, 
+  MatSortModule, 
+  MatDialogModule, 
+  MatButtonModule 
+} from '@angular/material';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSortModule,
+    MatDialogModule,
+    MatButtonModule,
   ],
   declarations: [NotesListComponent, NoteDetailComponent, DataTableComponent],
   providers: [NotesService]
